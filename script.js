854ab289ec6b77ad32dbd5a55cb6053a1266e01b
Lubenem <Lubenemkrkm@gmail.com>
2021-09-29 18:54:44 +0300

    add-task-button

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-let age = 30;
-console.log("Hello World!");
+window.onload = function () {
+  document
+    .querySelector(".add-task-button")
+    .addEventListener("click", function () {
+      let taskText = document.querySelector(".task-text");
+      if (!taskText.classList.contains("hiden")) {
+        document.querySelector(".task-text").className += " hiden";
+      } else {
+        document.querySelector(".task-text").className = "task-text";
+      }
+    });
+};
+
+//   "click",
+//   function () {
+//     document.querySelector(".task-text").className += "hiden";
+//     console.log("hello");
+//   },
+//   false
+// );
